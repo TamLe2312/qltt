0a685ce5cdcf3548359b1da5ee1b2c40bcbd118f
MichaelTam <tamle23122004@gmail.com>
2025-09-21 19:03:59 +0700

    update orderController

--- server/src/controllers/orderControllers.js
+++ server/src/controllers/orderControllers.js
@@ -13,10 +13,7 @@ const getAllOrders = async (req, res) => {
 const getOrderById = async (req, res) => {
   try {
     const { id } = req.params;
-    const result = await pool.query(
-      "SELECT * FROM orders WHERE order_id = $1",
-      [id]
-    );
+    const result = await pool.query("SELECT * FROM orders WHERE id = $1", [id]);
     if (result.rows.length === 0) {
       return res.status(404).json({ message: "Order not found" });
     }
@@ -29,20 +26,17 @@ const getOrderById = async (req, res) => {
 
 const createOrder = async (req, res) => {
   try {
-    const { customer_id, branch_id, notes, metadata, products } = req.body;
+    const { customer_address_id, user_id, branch_id, note, products } =
+      req.body;
 
     const result = await pool.query(
       `SELECT * FROM create_order($1, $2, $3, $4, $5)`,
-      [
-        customer_id,
-        branch_id,
-        notes,
-        JSON.stringify(metadata),
-        JSON.stringify(products),
-      ]
+      [customer_address_id, user_id, branch_id, note, JSON.stringify(products)]
     );
 
-    res.status(201).json(result.rows[0]);
+    res.status(201).json({ message: "Order created" });
+    // console.log(req.body);
+    // res.status(201).json(req.body);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
@@ -52,15 +46,43 @@ const createOrder = async (req, res) => {
 const updateOrder = async (req, res) => {
   try {
     const { id } = req.params;
-    const { notes, metadata, products } = req.body;
+    const {
+      customer_address_id,
+      customer_id,
+      status,
+      note,
+      products,
+      street,
+      ward,
+      district,
+      city,
+      country,
+      zipcode,
+    } = req.body;
+
     const result = await pool.query(
-      `SELECT * FROM update_order($1, $2, $3, $4)`,
-      [id, notes, JSON.stringify(metadata), JSON.stringify(products)]
+      `SELECT * FROM update_order($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
+      [
+        id,
+        customer_address_id,
+        customer_id,
+        status,
+        note,
+        JSON.stringify(products),
+        street,
+        ward,
+        district,
+        city,
+        country,
+        zipcode,
+      ]
     );
+
     if (result.rows.length === 0) {
       return res.status(404).json({ message: "Order not found" });
     }
-    res.json(result.rows[0]);
+
+    res.json({ message: "Order updated" });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
@@ -71,13 +93,13 @@ const deleteOrder = async (req, res) => {
   try {
     const { id } = req.params;
     const result = await pool.query(
-      "DELETE FROM orders WHERE order_id = $1 RETURNING *",
+      "DELETE FROM orders WHERE id = $1 RETURNING *",
       [id]
     );
     if (result.rows.length === 0) {
       return res.status(404).json({ message: "Order not found" });
     }
-    res.json({ message: "Order deleted", order: result.rows[0] });
+    res.json({ message: "Order deleted" });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
